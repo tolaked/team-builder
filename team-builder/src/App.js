fcb47dc1afa21142fbe0fded50aba05697b59f93
Akere Adetola <adetolaakere1@gmail.com>
2019-09-18 15:12:16 +0100

    rendered form component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import Form from "./Components/FormComponent";
 
 const myMembers = [
-  { id: uuid(), name: "Tola", role: "Front-end Engineer", age: "20" },
-  { id: uuid(), name: "Reyes", role: "Back-end Engineer", age: "43" },
+  { id: uuid(), name: "Tola", role: "Front-end ", age: "20" },
+  { id: uuid(), name: "Reyes", role: "Back-end ", age: "43" },
   { id: uuid(), name: "Rachael", role: "UI/UX", age: "29" }
 ];
 
 const initialMember = {
   name: "",
+  role: "",
   age: ""
 };
 
@@ -18,16 +19,56 @@ function App() {
   const [memberList, setMemberList] = useState(myMembers);
   const [initialList, setInitialList] = useState(initialMember);
 
-  const onMemberChange = e => {
-    setMemberList({ ...memberList, [e.target.identity]: e.target.value });
+  const onNameChange = e => {
+    setInitialList({
+      name: e.target.value,
+      role: initialList.role,
+      age: initialList.age
+    });
+  };
+
+  const onRole = e => {
+    setInitialList({
+      name: initialList.name,
+      role: e.target.value,
+      age: initialList.age
+    });
+  };
+
+  const OnAgeChange = e => {
+    setInitialList({
+      name: initialList.name,
+      role: initialList.role,
+      age: e.target.value
+    });
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const newMember = {
+      id: uuid(),
+      name: initialList.name,
+      role: initialList.role,
+      age: initialList.age
+    };
+    const updatedMember = memberList.concat(newMember);
+    setMemberList(updatedMember);
+    setInitialList(initialMember);
   };
 
   return (
     <div className="App">
-      <Form onMemberChange={onMemberChange} />
-      {myMembers.map(member => (
+      <Form
+        OnAgeChange={OnAgeChange}
+        onNameChange={onNameChange}
+        onRole={onRole}
+        initialList={initialList}
+        onFormSubmit={onFormSubmit}
+      />
+      {memberList.map(member => (
         <h5 key={member.id}>
-          {member.name} is {member.age} years old.
+          {member.name} is a {member.role} engineer. Age is {member.age} years
+          old.
         </h5>
       ))}
     </div>
--- team-builder/src/Components/FormComponent.js
+++ team-builder/src/Components/FormComponent.js
@@ -1,30 +1,41 @@
 import React from "react";
 
 function FormComponent(props) {
-  const { onMemberChange } = props;
+  const { OnAgeChange, onNameChange, onRole, onFormSubmit } = props;
+  const { name, age, role } = props.initialList;
+  const isDisabled = () => {
+    if (!name || !age || !role) {
+      return true;
+    }
+  };
+
   return (
     <div>
       <form>
         <label htmlFor="nameInput">Name</label>
         <input
           maxLength={50}
-          onChange={onMemberChange}
+          onChange={onNameChange}
           id="nameInput"
+          value={name}
           type="text"
         />
 
         <label htmlFor="nameInput">Role</label>
         <input
           maxLength={50}
-          onChange={onMemberChange}
+          onChange={onRole}
           id="nameInput"
+          value={role}
           type="text"
         />
 
         <label htmlFor="ageInput">Age</label>
-        <input onChange={onMemberChange} id="ageInput" type="number" />
+        <input onChange={OnAgeChange} id="ageInput" value={age} type="number" />
 
-        <button>submit</button>
+        <button disabled={isDisabled()} onClick={onFormSubmit}>
+          submit
+        </button>
       </form>
     </div>
   );
